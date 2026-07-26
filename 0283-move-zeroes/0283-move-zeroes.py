f067018b9b1de37bc731a63f58ade3505dbeddd1
SHRUTHI DURGAA C <shrudurga08@gmail.com>
2026-07-26 21:09:35 +0530

    Time: 3 ms (90.14%), Space: 13.6 MB (19.6%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+
+        j = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[j] = nums[i]
+                j += 1
+
+        while j < len(nums):
+            nums[j] = 0
+            j += 1      

e205b3a8d395c55dcc45383a842005f80ce51727
SHRUTHI DURGAA C <shrudurga08@gmail.com>
2026-08-03 14:49:11 +0530

    Time: 4 ms (86.59%), Space: 12.7 MB (66.54%) - LeetHub

---
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findDifference(self, nums1, nums2):
+
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        answer1 = list(set1 - set2)
+        answer2 = list(set2 - set1)
+
+        return [answer1, answer2]
